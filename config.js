3f397780b5fd344413e795ec17dbc3575e175d64
Sindhuja R <54587799+Sindhuja-R-21@users.noreply.github.com>
2022-01-12 19:46:43 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD7IkywCZjyTI9xNVRqof6NvVtpfHkQJWE",
+  authDomain: "teststory-c3f62.firebaseapp.com",
+  projectId: "teststory-c3f62",
+  storageBucket: "teststory-c3f62.appspot.com",
+  messagingSenderId: "460076816949",
+  appId: "1:460076816949:web:18a3ec39b1d530b797af7c"
+  };
